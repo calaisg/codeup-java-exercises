3fc7dc6ffb0ddce1cf2133ba078b65dead499e35
Calais Galbraith <calaisgalbraith@gmail.com>
2021-08-03 11:25:28 -0500

    finished bob exercise - fixed empty input error

--- src/Bob.java
+++ src/Bob.java
@@ -7,21 +7,22 @@ public class Bob {
     //Bob is a lackadaisical teenager. In conversation, his responses are very limited.
         // DONE: Bob answers 'Sure.' if you ask him a question. (the input ends with a question mark)
         //DONE: He answers 'Whoa, chill out!' if you yell at him. (the input ends with an exclamation mark)
-        //TODO: He says 'Fine. Be that way!' if you address him without actually saying anything. (empty input)
-            //TODO: can't pass empty input?
+        //DONE: He says 'Fine. Be that way!' if you address him without actually saying anything. (empty input)
         //DONE: He answers 'Whatever.' to anything else.
     //Write the Java code necessary so that a user of your command line application can have a conversation with Bob.
 
     Scanner sc = new Scanner(System.in);
     System.out.println("I'm Bob...do you need something? (Y/N)");
-    String answer = sc.next();
+    String answer = sc.nextLine();
 
     if(answer.equals("Y") || answer.equals("y")){
         boolean checking = true;
         while(checking){
             System.out.println("Ugh...what do you need?");
-            String input = sc.next();
-            if (input.equals(" ")){
+            String input = sc.nextLine();
+            //next looks for token (non whitespace)
+            //next line to fix (?) see what gives you
+            if (input.equals("")){
                 System.out.println("Fine. Be that way!");
             }
             else if(input.endsWith("?")){
@@ -34,8 +35,8 @@ public class Bob {
                 System.out.println("Whatever.");
             };
 
-            System.out.println("..do you need something else? (Y/N)");
-            String stillGoing = sc.next();
+            System.out.println("...do you need something else? (Y/N)");
+            String stillGoing = sc.nextLine();
             if(!stillGoing.equals("Y") && !stillGoing.equals("y")){
                 System.out.println(".....get out of my room");
                 System.exit(0);
