e40fd6575b41d9f3ce303a2e6f40c50ed3a0b1ae
Calais Galbraith <calaisgalbraith@gmail.com>
2021-08-06 23:28:06 -0500

    created arrayExercises.java - complete when go over in class

---
+++ src/ArraysExercises.java
@@ -0,0 +1,35 @@
+public class ArraysExercises {
+
+    public static void main(String [] args){
+
+        //1 Array Basics
+            //run the following code. What happens? --> weird stuff printed, need the .toString
+                //int[] numbers = {1, 2, 3, 4, 5};
+                //System.out.println(numbers);
+            //Create an array that holds 3 Person objects.
+        // Assign a new instance of the Person class to each element.
+        // Iterate through the array and print out the name of each person in the array.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
