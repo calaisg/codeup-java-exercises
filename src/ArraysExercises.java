6033ca9e02d19af8dbf6bcddc029b97be50c3feb
calaisgalbraith <calaisgalbraith@gmail.com>
2021-08-10 13:53:59 -0500

    added addPerson

--- src/ArraysExercises.java
+++ src/ArraysExercises.java
@@ -1,46 +1,43 @@
+import java.util.Arrays;
+
 public class ArraysExercises {
 
-    public static void main(String [] args){
+    public static void main(String[] args) {
 
         //1 Array Basics
-            //run the following code. What happens? --> weird stuff printed, need the .toString
-                //int[] numbers = {1, 2, 3, 4, 5};
-                //System.out.println(numbers);
+        //run the following code. What happens? --> weird stuff printed, need the .toString
+        //int[] numbers = {1, 2, 3, 4, 5};
+        //System.out.println(numbers);
 
-            //Create an array that holds 3 Person objects.
-            Person[] people= new Person[3];
+        //Create an array that holds 3 Person objects.
+        Person[] people = new Person[3];
 
         // Assign a new instance of the Person class to each element.
-            people[0] = new Person("Chanel");
-            people[1] = new Person("Jubilee");
-            people[2] = new Person("Nala");
+        people[0] = new Person("Chanel");
+        people[1] = new Person("Jubilee");
+        people[2] = new Person("Nala");
 
         // Iterate through the array and print out the name of each person in the array.
-            for(Person individualPerson : people){
-                System.out.println(individualPerson.getName());
-            }
-            //need get name, was getting reference to object
+        for (Person individualPerson : people) {
+            System.out.println(individualPerson.getName());
+        }
+        //need get name, was getting reference to object
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        people = addPerson(people, new Person("Cocoa"));
+        System.out.println("");
+        System.out.println("New Array");
+        for (Person individualPerson : people) {
+            System.out.println(individualPerson.getName());
+        }
 
     }
+
+    public static Person[] addPerson(Person[] people, Person personToAdd) {
+        Person[] newPeopleArray = Arrays.copyOf(people, people.length + 1);
+
+        newPeopleArray[newPeopleArray.length - 1] = personToAdd;
+
+        return newPeopleArray;
+    }
+
 }
