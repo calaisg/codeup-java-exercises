253a7811492b3290200a924a7e889c2f8e8d4b8b
calaisgalbraith <calaisgalbraith@gmail.com>
2021-08-12 16:33:53 -0500

    some spaces were added unsure where lol

--- src/util/Input.java
+++ src/util/Input.java
@@ -112,7 +112,6 @@ public class Input {
 //        System.out.println(input.getString());
 //        System.out.println(input.yesNo());
 //        System.out.println(input.getInt(1, 10));
-//            System.out.println(input.getInt());
 //        System.out.println(input.getDouble(1.0, 10.0));
 //        System.out.println(input.getDouble());
 
